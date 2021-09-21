1d164abdb3026cf410c973a6d52edc868e328356
DiegoSullon <jdiego.sullon@gmail.com>
2021-09-21 17:36:03 -0500

    getPokemons

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Main = () => {
-  return <div>Holaaaaa</div>
+  const [page, setPage] = useState(0)
+  const [pokemons, setPokemons] = useState([])
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon-species?offset=${page}&limit=20`)
+      .then(response => {
+        console.log(response.data.results)
+        setPokemons(response.data.results)
+      })
+  }, [page])
+  return (
+    <main>
+      <h1>Pokedex</h1>
+      <section>{pokemons && pokemons.map(p => <div key={pokemons.indexOf(p)}>{p.name}</div>)}</section>
+    </main>
+  )
 }
 
 export default Main
