cd837d5d5770dc82cbb3ba55e80d8bbbe2454d77
DiegoSullon <jdiego.sullon@gmail.com>
2021-09-21 20:10:52 -0500

    get pokemon and evolution chain

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { MainContainer, ResultSection, P } from '../styles'
 import Pagination from '../../components/pagination/Pagination'
-import { getPokemonId } from '../../util/strings'
+import { getPokemonId } from '../../util/utils'
 import CardLink from '../../components/cardLink/CardLink'
+import Spinner from '../../components/spinner/Spinner'
 
 const Main = () => {
   const size = 20
   const [page, setPage] = useState(0)
   const [pokemons, setPokemons] = useState([])
   const [totalElements, setTotalElements] = useState(0)
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
+    setLoading(true)
     axios
       .get(
         `https://pokeapi.co/api/v2/pokemon-species?offset=${page}&limit=${size}`
       )
       .then(response => {
         setTotalElements(response.data.count)
+        setLoading(false)
         setPokemons(response.data.results)
       })
   }, [page])
@@ -25,6 +29,7 @@ const Main = () => {
   }
   return (
     <MainContainer>
+      {loading && <Spinner />}
       <h1>Pokedex</h1>
       <P>
         Page {page + 1} of {parseInt(totalElements / size) + 1}
