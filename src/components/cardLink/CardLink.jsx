4a728819f710dcfe023b7da0561d80ba0925ade1
DiegoSullon <jdiego.sullon@gmail.com>
2021-09-27 01:39:57 -0500

    Pokemon card styles

--- src/components/cardLink/CardLink.jsx
+++ src/components/cardLink/CardLink.jsx
@@ -1,12 +1,57 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { upperFirts } from '../../util/utils'
-import { Card } from './styles'
+import {
+  AbsoluteDiv,
+  Card,
+  ImgContainer,
+  Name,
+  OrderTagContent,
+  StatGroup,
+  StatsGrid
+} from './styles'
 
-const CardLink = ({ name, pokemonId }) => {
+const CardLink = ({ name, pokemonId, sprite }) => {
   return (
     <Link to={`/${pokemonId}`}>
-      <Card>{upperFirts(name)}</Card>
+      <Card>
+        <ImgContainer>
+          <img src={sprite} alt='default' />
+          <AbsoluteDiv>
+            <OrderTagContent>
+              <label htmlFor='order'>Order:</label>
+              <span>30</span>
+            </OrderTagContent>
+          </AbsoluteDiv>
+        </ImgContainer>
+        <Name>{upperFirts(name)}</Name>
+        <StatsGrid>
+          <StatGroup>
+            <label htmlFor='hp'>HP:</label>
+            <span>30</span>
+          </StatGroup>
+          <StatGroup>
+            <label htmlFor='hp'>HP:</label>
+            <span>30</span>
+          </StatGroup>
+          <StatGroup>
+            <label htmlFor='hp'>HP:</label>
+            <span>30</span>
+          </StatGroup>
+          <StatGroup>
+            <label htmlFor='hp'>HP:</label>
+            <span>30</span>
+          </StatGroup>
+          <StatGroup>
+            <label htmlFor='hp'>HP:</label>
+            <span>30</span>
+          </StatGroup>
+          <StatGroup>
+            <label htmlFor='hp'>HP:</label>
+            <span>30</span>
+          </StatGroup>
+        </StatsGrid>
+      </Card>
     </Link>
   )
 }
